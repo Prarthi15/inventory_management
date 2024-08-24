50d8cef26cf8cb0adcd030532d209a49e644aa7f
divyanshpatidar <divyanshpatidar85@gmail.com>
2024-08-24 18:14:44 +0530

    provider update

--- lib/Api/order-page-checkbox-provider.dart
+++ lib/Api/order-page-checkbox-provider.dart
@@ -2,20 +2,34 @@ import 'package:flutter/material.dart';
 
 class CheckBoxProvider with ChangeNotifier{
   List<bool> _listCheckBox = List.generate(5, (index) => false);
+  List<bool> _textFieldEnabler1 = List.generate(5, (index) => false);
+  List<bool> _textFieldEnabler2 = List.generate(5, (index) => false);
   bool _mainCheckBox = false;
 
   get mainCheckBox=>_mainCheckBox;
   List<bool> get checkboxStates=>_listCheckBox;
+  List<bool> get textFieldEnabler1=>_textFieldEnabler1;
+  List<bool> get textFieldEnabler2=>_textFieldEnabler2;
 
   void upDateMainCheckBox(bool val){
-    // print("i am calling by dipu $val");
+    
     _mainCheckBox=val;
      _listCheckBox = List.generate(5, (index) => val);
      notifyListeners();
   }
    void updateListCheckBox(bool val,int index){
-    // _mainCheckBox=val;
+    
     _listCheckBox[index]=val;
+    notifyListeners();
+  }
+   void updatetextFieldEnabler1(bool val,int index){
+    
+    _textFieldEnabler1[index]=val;
+    notifyListeners();
+  }
+   void updatetextFieldEnabler2(bool val,int index){
+    
+    _textFieldEnabler2[index]=val;
     notifyListeners();
   }
 }
