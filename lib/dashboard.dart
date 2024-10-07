d40c527f1594c7aa9dd220c2488b4ac43f09e53b
divyanshpatidar <divyanshpatidar85@gmail.com>
2024-10-07 16:26:13 +0530

    dasboard main menu colors showing when it's collapsed and now update on ui on show label page

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -289,7 +289,7 @@ class _DashboardPageState extends State<DashboardPage> {
       ),
       child: ExpansionTile(
         tilePadding: const EdgeInsets.symmetric(horizontal: 20.0),
-         collapsedBackgroundColor:["Orders Page","Book Page","Picker Page","Packer Page","Checker Page","Racked Page","Manifest Page"].contains(selectedDrawerItem)?AppColors.blueAccent:AppColors.white,
+         collapsedBackgroundColor:["Orders Page","Book Page","Picker Page","Packer Page","Checker Page","Racked Page","Manifest Page"].contains(selectedDrawerItem)?Colors.blue.withOpacity(0.2):AppColors.white,
         title: Text(
           'Orders',
           style: TextStyle(
@@ -411,7 +411,7 @@ class _DashboardPageState extends State<DashboardPage> {
       ),
       child: ExpansionTile(
         tilePadding: const EdgeInsets.symmetric(horizontal: 20.0),
-         collapsedBackgroundColor:["Manage Inventory"].contains(selectedDrawerItem)?AppColors.blueAccent:AppColors.white,
+         collapsedBackgroundColor:["Manage Inventory"].contains(selectedDrawerItem)?Colors.blue.withOpacity(0.2):AppColors.white,
         title: Text(
           'Inventory',
           style: TextStyle(
@@ -460,7 +460,7 @@ class _DashboardPageState extends State<DashboardPage> {
       ),
       child: ExpansionTile(
         tilePadding: const EdgeInsets.symmetric(horizontal: 20.0),
-        collapsedBackgroundColor:["Label Page","Create Label Page","Product Master","Category Master","Combo Master","Marketplace Master","Location Master"].contains(selectedDrawerItem)?AppColors.blueAccent:AppColors.white,
+        collapsedBackgroundColor:["Label Page","Create Label Page","Product Master","Category Master","Combo Master","Marketplace Master","Location Master"].contains(selectedDrawerItem)?Colors.blue.withOpacity(0.2):AppColors.white,
         
         
         title: Text(
